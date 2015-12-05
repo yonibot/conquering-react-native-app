df862fde98014edb58253ab7bf8a70a7d0cc66a8
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2015-12-05 18:35:35 +0200

    factor out items list

---
+++ components/ItemsList.js
@@ -0,0 +1,85 @@
+import React from 'react-native';
+
+var {
+  ListView,
+  View,
+  Text,
+  TouchableHighlight,
+  Image,
+  StyleSheet,
+} = React;
+
+class ItemsList extends React.Component{
+  renderItem(item) {
+    const { deleteItem, toggleCompleted } = this.props;
+    return (
+        <View>
+          <View style={styles.listItem}>
+            <Text
+              style={[styles.itemText, item.completed && styles.completedItemText]}
+              onPress={toggleCompleted.bind(undefined, item)}>
+              {item.content}
+            </Text>
+            <TouchableHighlight
+              onPress={deleteItem.bind(undefined, item)}
+              underlayColor={'#324B66'}>
+              <Image
+              style={styles.icon}
+              source={{uri: 'https://cdn0.iconfinder.com/data/icons/ikooni-outline-free-basic/128/free-27-32.png'}}
+              />
+            </TouchableHighlight>
+          </View>
+          <View style={styles.separator}></View>
+        </View>
+    );
+  }
+
+  render() {
+    return (
+      <ListView
+        style={styles.listStyle}
+        dataSource={this.props.dataSource}
+        renderRow={this.renderItem.bind(this)}
+        />
+    )
+  }
+}
+
+ItemsList.propTypes = {
+  dataSource: React.PropTypes.object.isRequired
+}
+
+var styles = StyleSheet.create({
+  listStyle: {
+    backgroundColor: '#324B66',
+    flex: 1
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 12,
+  },
+  icon: {
+    width: 25,
+    height: 25,
+  },
+  itemText: {
+    fontSize: 20,
+    color: 'white',
+    paddingLeft: 5,
+    flex: 0.5
+  },
+  completedItemText: {
+    textDecorationLine: 'line-through',
+    textDecorationColor: 'black'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#47729E'
+  },
+})
+
+export { ItemsList as default };
--- containers/TodoList.js
+++ containers/TodoList.js
@@ -6,16 +6,16 @@ import ApiUtils from '../utils/ApiUtils';
 import Api from '../utils/Api'
 import Colours from '../utils/Colours'
 import TodoForm from '../components/TodoForm'
+import ItemsList from '../components/ItemsList'
 
 var {
   Text,
   View,
   AsyncStorage,
-  ListView,
   StyleSheet,
   TextInput,
   TouchableHighlight,
-  Image,
+  ListView,
 } = React;
 
 class TodoList extends React.Component{
@@ -89,29 +89,6 @@ class TodoList extends React.Component{
     this.setState({todo})
   }
 
-  renderItem(item) {
-    return (
-        <View>
-          <View style={styles.listItem}>
-            <Text
-              style={[styles.itemText, item.completed && styles.completedItemText]}
-              onPress={this.toggleCompleted.bind(this, item)}>
-              {item.content}
-            </Text>
-            <TouchableHighlight
-              onPress={this.deleteItem.bind(this, item)}
-              underlayColor={'#324B66'}>
-              <Image
-              style={styles.icon}
-              source={{uri: 'https://cdn0.iconfinder.com/data/icons/ikooni-outline-free-basic/128/free-27-32.png'}}
-              />
-            </TouchableHighlight>
-          </View>
-          <View style={styles.separator}></View>
-        </View>
-    );
-  }
-
   render() {
     return (
       <View style={styles.todoListPage}>
@@ -119,11 +96,10 @@ class TodoList extends React.Component{
           addTodo={this.addTodo.bind(this)}
           onUpdate={this.updateTodo.bind(this)}
           todo={this.state.todo} />
-        <ListView
-          style={styles.listStyle}
+        <ItemsList
           dataSource={this.state.dataSource}
-          renderRow={this.renderItem.bind(this)}
-          />
+          toggleCompleted={this.toggleCompleted.bind(this)}
+          deleteItem={this.deleteItem.bind(this)} />
         <LogoutFooter logout={this.logout.bind(this)} />
       </View>
     )
@@ -133,36 +109,6 @@ class TodoList extends React.Component{
 var styles = StyleSheet.create({
   todoListPage: {
     flex: 1,
-  },
-  listStyle: {
-    backgroundColor: '#324B66',
-    flex: 1
-  },
-  listItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingRight: 20,
-    paddingTop: 10,
-    paddingBottom: 12,
-  },
-  icon: {
-    width: 25,
-    height: 25,
-  },
-  itemText: {
-    fontSize: 20,
-    color: 'white',
-    paddingLeft: 5,
-    flex: 0.5
-  },
-  completedItemText: {
-    textDecorationLine: 'line-through',
-    textDecorationColor: 'black'
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#47729E'
   },
 });
 

2c715a5857963e10fe9a85f7fb45473033fc7730
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2015-12-09 09:57:32 +0200

    add router

--- index.ios.js
+++ index.ios.js
@@ -6,6 +6,8 @@
 
 import React from 'react-native';
 import TodoList from './containers/TodoList';
+import Router from 'gb-native-router';
+import Colours from './utils/Colours'
 
 var {
   AppRegistry,
@@ -13,11 +15,23 @@ var {
 
 class AsyncStorageTest extends React.Component{
   render() {
+    let firstRoute = {
+      component: TodoList,
+      name: 'TodoList'
+    }
     return (
-      <TodoList />
+      <Router
+        firstRoute={firstRoute}
+        headerStyle={styles.navBar} />
     );
   }
 }
+
+var styles = StyleSheet.create({
+  navBar: {
+    backgroundColor: Colours.cokeFizz,
+  }
+});
 
 
 AppRegistry.registerComponent('AsyncStorageTest', () => AsyncStorageTest);
